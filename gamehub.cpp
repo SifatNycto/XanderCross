28d447f5dce2d6e6cdcb810e2ad2300fb591651c
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-03-26 11:22:33 +0600

    Added a simple number guessing game and updated the .gitignore file

--- gamehub.cpp
+++ gamehub.cpp
@@ -4,30 +4,82 @@
 
 int main()
 {
-    int num, guess, tries = 0;
-    srand(time(NULL));
-    num = (rand() % 100) + 1;
-    std::cout << "\n+--------------------------------------+";
-    std::cout << "\n|>>>      NUMBER GUESSING GAME      <<<|";
-    std::cout << "\n+--------------------------------------+";
-    
-    std::cout << std::endl;
-    
-    do
-    {
-        std::cout << "\nEnter number guess between (1-100): ";
-        std::cin >> guess;
-        tries++;
+    int secretNumber;
+    int guess;
+    int attempts = 0;
 
-        if (guess > num) std::cout << '\n' << "Guess is too high.";
-        else if (guess < num) std::cout << '\n' << "Guess is too low..";
+    srand(time(NULL));
+    secretNumber = (rand() % 100) + 1;
+
+    // Game Loop
+    while(guess != secretNumber)
+    {
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber)
+            std::cout << "Too High";
+
+        else if (guess < secretNumber)
+            std::cout << "Too Low";
+
         else
         {
-            std::cout << "\n\n" << "CORRECT GUESS!!!";
-            std::cout << '\n' << "You tried " << tries << " times.";
+            std::cout << "Correct! You guessed correct" << std::endl;
+            std::cout << "Attempts: " << attempts;
+            break;
         }
-        
-    } while (guess != num);
-    
-    return 0;
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main()
+// {
+//     int num, guess, tries = 0;
+//     srand(time(NULL));
+//     num = (rand() % 100) + 1;
+//     std::cout << "\n+--------------------------------------+";
+//     std::cout << "\n|>>>      NUMBER GUESSING GAME      <<<|";
+//     std::cout << "\n+--------------------------------------+";
+    
+//     std::cout << std::endl;
+    
+//     do
+//     {
+//         std::cout << "\nEnter number guess between (1-100): ";
+//         std::cin >> guess;
+//         tries++;
+
+//         if (guess > num) std::cout << '\n' << "Guess is too high.";
+//         else if (guess < num) std::cout << '\n' << "Guess is too low..";
+//         else
+//         {
+//             std::cout << "\n\n" << "CORRECT GUESS!!!";
+//             std::cout << '\n' << "You tried " << tries << " times.";
+//         }
+        
+//     } while (guess != num);
+    
+//     return 0;
+// }

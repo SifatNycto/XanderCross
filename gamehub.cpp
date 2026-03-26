9b8890e1e7ab6c3a88030ee34e706777501f18a2
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-03-26 13:17:01 +0600

    Converted the Number Guessing Game in OOP concept with proper initialization

--- gamehub.cpp
+++ gamehub.cpp
@@ -2,60 +2,57 @@
 #include <cstdlib>
 #include <ctime>
 
-void header_ngg();
+
+class NumberGuessGame {
+    private:
+        int secretNumber;
+        int guess = 0;
+        int attempts = 0;
+
+    public:
+       NumberGuessGame() {
+        srand(time(NULL));
+        secretNumber = (rand() % 100) + 1;
+       }
+        
+        void header_ngg() {
+            std::cout << "\n+--------------------------------------+";
+            std::cout << "\n|>>>      NUMBER GUESSING GAME      <<<|";
+            std::cout << "\n+--------------------------------------+";
+            std::cout << std::endl;
+        }
+        void play() {
+            
+            // NGG Game Header
+            header_ngg();
+            std::cout << "Guess a number between 1 to 100" << std::endl;
+
+            // Game Loop
+            while(guess != secretNumber)
+            {
+                std::cout << "Enter your guess: ";
+                std::cin >> guess;
+                attempts++;
+
+                if (guess > secretNumber)
+                    std::cout << "Too High" << std::endl;
+
+                else if (guess < secretNumber)
+                    std::cout << "Too Low" << std::endl;
+
+                else
+                {
+                    std::cout << "Correct! You guessed correct" << std::endl;
+                    std::cout << "Attempts: " << attempts;
+                }
+            }
+        }
+};
+
 int main()
 {
-    int secretNumber;
-    int guess;
-    int attempts = 0;
-
-    srand(time(NULL));
-    secretNumber = (rand() % 100) + 1;
+   NumberGuessGame ngg;
+   ngg.play();
     
-    // NGG Game Header
-    header_ngg();
-    std::cout << "Guess a number between 1 to 100";
-
-    // Game Loop
-    while(guess != secretNumber)
-    {
-        std::cout << "Enter your guess: ";
-        std::cin >> guess;
-        attempts++;
-
-        if (guess > secretNumber)
-            std::cout << "Too High";
-
-        else if (guess < secretNumber)
-            std::cout << "Too Low";
-
-        else
-        {
-            std::cout << "Correct! You guessed correct" << std::endl;
-            std::cout << "Attempts: " << attempts;
-            break;
-        }
-
-    }
+   return 0;
 }
-
-void header_ngg() {
-    std::cout << "\n+--------------------------------------+";
-    std::cout << "\n|>>>      NUMBER GUESSING GAME      <<<|";
-    std::cout << "\n+--------------------------------------+";
-    std::cout << std::endl;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-

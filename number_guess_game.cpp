08cc50887ae11ab0fe0b08aa879e98d96f66f5ce
Mehedi Hasan Sifat <165242401+SifatNycto@users.noreply.github.com>
2026-04-03 16:11:33 +0600

    A review day just

--- number_guess_game.cpp
+++ number_guess_game.cpp
@@ -87,6 +87,7 @@ class NumberGuessGame {
                         }
                     }
 
+                    // ................
                     attempts++;
 
                     if (guess > secretNumber)
